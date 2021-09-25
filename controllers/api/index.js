3ffebcb005e5f85733af5bb4e36c5d5da81e5231
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-09-25 12:54:16 -0600

    inital add

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+// require all logic from other pages 
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
